ebab2466ae0aeedb9c821d958f20a4f95a5d88c4
Ксения Седова <sedovakseniya1506@gmail.com>
2023-08-15 22:45:46 +0300

    выполнение тз, замена шаблонов

--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -7,10 +7,9 @@ from core.models import PublishedModel
 def ice_cream_detail(request, pk):
     template = 'ice_cream/detail.html'
     ice_cream = get_object_or_404(
-        IceCream.objects.values('title', 'description').filter(
-        is_published=True),
-        pk=pk)
-    
+        IceCream.objects.filter(
+        is_published=True, category__is_published=True),
+        pk=pk) 
     context = {
         'ice_cream': ice_cream,
     }
@@ -19,5 +18,10 @@ def ice_cream_detail(request, pk):
 
 def ice_cream_list(request):
     template = 'ice_cream/list.html'
-    context = {}
+    ice_cream_list = IceCream.objects.select_related(
+        'category').filter(
+            is_published=True,
+            category__is_published=True
+            ).order_by('category')
+    context = {'ice_cream_list': ice_cream_list}
     return render(request, template, context)
